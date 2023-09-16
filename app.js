61554fc4b9eb5528c2fcf0c1d704e429c2c36096
hitesh <hiteshwadile2003@gmail.com>
2023-09-16 07:07:26 +0530

    Schema for the Database is Created

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ require('./db/db').connect()
 const express = require('express');
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.post('/register', async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
 })
 module.exports = app
